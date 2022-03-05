3586b646cb27b87423c40114b75713165cc1ca7a
Larry May <larrymayster@gmail.com>
2022-03-05 11:44:40 -0500

    Demo issue with kicad on unstable

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.kicad
+          ];
+        };
+      });
+}
